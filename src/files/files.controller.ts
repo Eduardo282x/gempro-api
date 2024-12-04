397b56b10d662e5425b0d9bd73e8b4cdaf94ee50
Eduardo282x <eduardokunmixyeah@gmail.com>
2024-12-04 01:30:10 -0400

    add multer to manage files

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,14 +1,55 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { File } from '@prisma/client';
+import { badResponse, DtoBaseResponse } from 'src/dtos/base.dto';
+
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { Observable, of } from 'rxjs';
+import { join } from 'path';
 
 @Controller('files')
 export class FilesController {
 
-    constructor(private filesServices: FilesService){}
+    constructor(private filesServices: FilesService) { }
 
     @Get()
-    async getFiles(): Promise<File[]>  {
+    async getFiles(): Promise<File[]> {
         return await this.filesServices.getFiles();
+    }
+
+    @Get('/:idFile')
+    async getActivities(@Param('idFile') idFile: string, @Res() res): Promise<Observable<Object> | DtoBaseResponse> {
+        try {
+            const fileName: File = await this.filesServices.findFile(idFile) as File;
+            if (fileName) {
+                return of(res.sendFile(join(process.cwd(), 'files_system/' + fileName.url)));
+            }
+        } catch(err) {
+            badResponse.message = 'Error al buscar el archivo';
+            return badResponse;
+        }
+    }
+
+    @Post()
+    @UseInterceptors(FileInterceptor('file', {
+        storage: diskStorage({
+            destination: './files_system',
+            filename: function (req, file, cb) {
+                cb(null, file.originalname);
+            },
+        }),
+    }))
+    async uploadFile(
+        @UploadedFile() file: Express.Multer.File,
+    ) {
+
+        // Verifica si el archivo se ha cargado correctamente.
+        if (!file) {
+            throw new BadRequestException('Archivo no encontrado');
+        }
+
+        // Continúa con la lógica de negocio (por ejemplo, guardar en la base de datos).
+        return await this.filesServices.uploadFiles(file);
     }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { File } from '@prisma/client';
+import { badResponse, baseResponse, DtoBaseResponse } from 'src/dtos/base.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
@@ -20,5 +21,34 @@ export class FilesService {
         })
 
         return files;
+    }
+
+    async findFile(idFile: string): Promise<File | DtoBaseResponse> {
+        const findFileName = await this.prismaService.file.findFirst({
+            where: {
+                id: Number(idFile)
+            }
+        });
+
+        return findFileName;
+    }
+
+    async uploadFiles(file: Express.Multer.File,): Promise<DtoBaseResponse> {
+        const saveFile = await this.prismaService.file.create({
+            data: {
+                name: file.filename,
+                url: file.filename,
+                uploadedById: 1,
+                directedToId: 1
+            }
+        });
+
+        if (!saveFile) {
+            badResponse.message = 'No se pudo guardar el archivo';
+            return badResponse;
+        }
+
+        baseResponse.message = 'Archivo subido exitosamente.';
+        return baseResponse;
     }
 }
